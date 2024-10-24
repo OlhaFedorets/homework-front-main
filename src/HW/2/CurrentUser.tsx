95962203117ee6c3482c868cd10368e74186f3f5
OlhaFedorets <fedoretsolha@gmail.com>
2024-10-24 15:43:44 +0300

    change CurrentUser

--- src/HW/2/CurrentUser.tsx
+++ src/HW/2/CurrentUser.tsx
@@ -1,8 +1,10 @@
 import { UserType } from './HW2';
 
 type CurrentUserPropsType = {
-  user: any // пропиши типизацию
+  user: UserType// пропиши типизацию
 };
+
+
 
 export const CurrentUser = (props: CurrentUserPropsType) => {
   return (
